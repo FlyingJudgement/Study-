6ee129c2eafc98f63e08cdaee88b74d8ad3bbb07
FlyingJudgement <flyingjudgement@gmail.com>
2021-02-19 07:22:20 +0000

    Added another item(shooe) from resources to inventory to slot[8] started the first save system breakdown

--- Assets/GameDev.tv Assets/Scripts/Inventories/Inventory.cs
+++ Assets/GameDev.tv Assets/Scripts/Inventories/Inventory.cs
@@ -138,6 +138,7 @@ namespace GameDevTV.Inventories
             slots = new InventoryItem[inventorySize];
             slots[0] = InventoryItem.GetFromID("71e73607-4bac-4e42-b7d6-5e6f91e92dc4");
             slots[1] = InventoryItem.GetFromID("0aa7c8b8-4796-42aa-89d0-9d100ea67d7b");
+            slots[8] = InventoryItem.GetFromID("cf0ab7c7-fe2a-4a6d-ad80-fea4a6c62707");
         }
 
         /// <summary>
